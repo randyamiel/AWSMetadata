f32c87f2bf28b8694c7d68f482064e6b95bb5671
Randy Amiel <randyamiel@users.noreply.github.com>
2018-08-31 20:46:35 -0400

    Update runLambda.sh

--- runLambda.sh
+++ runLambda.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# TODO: write a master shell script that runs the sub-routines such as runLambda etc..
+#       or we could use a config file.
+
 if [ $# -eq 0 ]; then
     echo "AWS VPC Machine name needs to be passed in as argument 1"
     exit 1
