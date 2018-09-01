b6824919765fe4a2553f0652067a6c07d7b8f0c6
Randy Amiel <randyamiel@users.noreply.github.com>
2018-08-31 21:37:04 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,41 +2,91 @@
 
 ##### Constants
 
-host="System Information for $HOSTNAME"
 right_now=$(date +"%x %r %Z")
-running_user="$USER"
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=4
-BACKTITLE="AWS MetaData 2018 - @rwnevinger @ramiel"
-TITLE="AWS Run Menu - $USER@$HOSTNAME"
-MENU="Choose one of the following options:"
 
-OPTIONS=(1 "CreateGateway"
-         2 "DeleteGateway"
-         3 "CreateRoute"
-         4 "DeleteRoute"
-         5 "runLambda")
+show_menu(){
+    NORMAL=`echo "\033[m"`
+    MENU=`echo "\033[36m"` #Blue
+    NUMBER=`echo "\033[33m"` #yellow
+    FGRED=`echo "\033[41m"`
+    RED_TEXT=`echo "\033[31m"`
+    ENTER_LINE=`echo "\033[33m"`
+    
+    
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}** ${NORMAL}AWS MetaData 2018 - @rwnevinger @ramiel ${MENU}**${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}** ${NORMAL} Running on $USER@$HOSTNAME - $right_now ${MENU}**${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Create Gateway ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Delete Gateway ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Create Route ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 4)${MENU} Delete Route ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU} run Lambda${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
+    read opt
+}
 
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
+function option_picked() {
+    COLOR='\033[01;31m' # bold red
+    RESET='\033[00;00m' # normal white
+    MESSAGE=${@:-"${RESET}Error: No message passed"}
+    echo -e "${COLOR}${MESSAGE}${RESET}"
+}
 
 clear
-case $CHOICE in
-        1)
-            #TODO read in gateway
-            echo "Creating Gateway..."
+show_menu
+while [ opt != '' ]
+    do
+    if [[ $opt = "" ]]; then
+            exit;
+    else
+        case $opt in
+        1) clear;
+        option_picked "Create a Gateway";
+        # use something like this to read in the gateway and other variables maybe set defaults
+   
+       # read -p "Select a VPC: " vpc;
+        #read -p "Select a subnet: " sub;
+        #read -p "Select a rtb: " rtb; 
+        #read -p "Select a mask [0.0.0.0/0]: " mask;
+        #java -cp .:target/AWSMetadata.jar com.bauenecp.CreateNatGateway $vpc $sub $rtb $mask;
+        java -version;
+        sleep 10;
+        menu;
+        ;;
+        2) clear;
+            option_picked "Option 2 Picked";
+            sudo mount /dev/sdi1 /mnt/usbDrive; 
+        menu;
             ;;
-        2)
-          #TODO read in gateway
-            echo "Deleting Gateway..."
+
+        3) clear;
+            option_picked "Option 3 Picked";
+        sudo service apache2 restart;
+            show_menu;
             ;;
-        3)
-            echo "Create Route..."
+
+        4) clear;
+            option_picked "Option 4 Picked";
+           ssh lmesser@ -p 2010;
+            show_menu;
             ;;
-esac
+         5) clear;
+            option_picked "Option 5 Picked";
+           ssh lmesser@ -p 2010;
+            show_menu;
+            ;; 
+        x)exit;
+        ;;
+        \n)exit;
+        ;;
+
+        *)clear;
+        option_picked "Pick an option from the menu";
+        show_menu;
+        ;;
+    esac
+fi
+done
